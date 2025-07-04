26f6c28d1eb6789a1bc8200fcd1fa55cfee13afb
vismey <vismeysen666@gmail.com>
2025-07-04 18:11:55 +0530

    WEBSITE LIVE

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARai2LEcUojb4Mkcbr9jfvsYfqb3kDBoQ",
+  authDomain: "fusician-41a4b.firebaseapp.com",
+  projectId: "fusician-41a4b",
+  storageBucket: "fusician-41a4b.firebasestorage.app",
+  messagingSenderId: "37855411530",
+  appId: "1:37855411530:web:8872ccd3a62fb5556395b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
